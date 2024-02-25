90bd395b283921ca2650262f7a12e14bef48c63a
unknown <emaneldeeb1999@gmail.com>
2024-02-25 13:53:52 +0200

    add files

---
+++ src/pipes/product.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'product',
+  standalone: true
+})
+export class ProductPipe implements PipeTransform {
+
+  transform(value: unknown, ...args: unknown[]): unknown {
+    return null;
+  }
+
+}
