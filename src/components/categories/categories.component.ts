7c2b8ef4121c7c156e62ad37d54cb259cd9d31e3
unknown <emaneldeeb1999@gmail.com>
2024-03-01 21:59:57 +0200

    Add category

--- src/components/categories/categories.component.ts
+++ src/components/categories/categories.component.ts
@@ -1,12 +1,33 @@
-import { Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { Component, OnInit, input } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { ProductsService } from '../../services/products.service';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-categories',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule, RouterModule],
+  providers: [ProductsService],
   templateUrl: './categories.component.html',
-  styleUrl: './categories.component.scss'
+  styleUrl: './categories.component.scss',
 })
-export class CategoriesComponent {
-
+export class CategoriesComponent implements OnInit {
+  cartItems: any[] = [];
+  constructor(
+    private PService: ProductsService,
+    private cartService: CartService
+  ) {
+    this.cartItems = this.cartService.getCartItems();
+    console.log(this.cartItems);
+  }
+  AllCategries: any;
+  ngOnInit(): void {
+    this.PService.getAllCategories().subscribe({
+      next: (data) => {
+        this.AllCategries = data;
+      },
+      error: (error) => {},
+    });
+  }
 }
