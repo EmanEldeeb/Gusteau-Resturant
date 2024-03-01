21518788b95a1e7eaa66d1939a20aa8677e8b7ad
Alaa Anees <alaaaneessaad@gmail.com>
2024-03-02 00:52:39 +0200

    cart ts added

--- src/components/cart/cart.component.ts
+++ src/components/cart/cart.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './cart.component.html',
-  styleUrl: './cart.component.scss'
+  styleUrl: './cart.component.scss',
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  cart: any;
+  cartItems: any[] = [];
+  cartTotalPrice = 0;
+  quantityValue: number = 0;
 
+  ngOnInit(): void {
+    const LSCart = localStorage.getItem('cart');
+    this.cart = LSCart ? JSON.parse(LSCart) : null;
+    this.cartItems = this.cart.items;
+    this.cartTotalPrice = this.cart.totalPrice;
+    console.log('ay 7aga', this.cartItems, this.cartTotalPrice);
+    console.log();
+  }
+  deleteFromCart(index: any) {
+    const LSCart = localStorage.getItem('cart');
+    this.cart = LSCart ? JSON.parse(LSCart) : null;
+    this.cart.totalPrice =
+      this.cart.totalPrice -
+      this.cartItems[index].price * this.cartItems[index].quantity;
+    this.cart.items.splice(index, 1);
+    this.cartItems = this.cart.items;
+    this.cartTotalPrice = this.cart.totalPrice;
+    localStorage.setItem('cart', JSON.stringify(this.cart));
+  }
+  modifieProductCount(quantity: any) {}
 }
