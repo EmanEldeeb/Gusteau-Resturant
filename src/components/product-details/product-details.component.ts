7b9513ed8aa38e90f2f6d8fde5501b9941e26ca7
unknown <emaneldeeb1999@gmail.com>
2024-03-02 00:41:11 +0200

    modifie add btn functionality

--- src/components/product-details/product-details.component.ts
+++ src/components/product-details/product-details.component.ts
@@ -69,35 +69,49 @@ export class ProductDetailsComponent implements OnInit {
   // }
   // addToCart(item: any) {
   // }
+
+  addToCart(item: any) {}
   addTOCart() {
     const cart = localStorage.getItem('cart');
     const oldCart = cart ? JSON.parse(cart) : null;
     if (!oldCart.items.length) {
+      console.log('first time');
       oldCart.items.push({
         quantity: this.quantityValue,
         id: this.mealDetails.id,
+        price: this.mealDetails.price,
+        name: this.mealDetails.name,
+        image: this.mealDetails.imageUrl,
       });
       oldCart.totalPrice =
         +oldCart.totalPrice + +this.mealDetails.price * this.quantityValue;
       localStorage.setItem('cart', JSON.stringify(oldCart));
     } else {
-      oldCart.items.forEach((item: any, index: any) => {
-        if (item.id == this.mealDetails.id) {
-          item.quantity = +item.quantity + +this.quantityValue;
-          oldCart.totalPrice =
-            +oldCart.totalPrice + +this.mealDetails.price * this.quantityValue;
-          localStorage.setItem('cart', JSON.stringify(oldCart));
-        } else if (item.id !== this.mealDetails.id) {
-          oldCart.items.push({
-            quantity: this.quantityValue,
-            id: this.mealDetails.id,
-          });
-          oldCart.totalPrice =
-            +oldCart.totalPrice + +this.mealDetails.price * this.quantityValue;
-          localStorage.setItem('cart', JSON.stringify(oldCart));
-        }
+      const itemExistsINDEX = oldCart.items.findIndex((item: any) => {
+        return item.id == this.mealDetails.id;
       });
+      const itemExists = oldCart.items[itemExistsINDEX];
+      if (itemExists) {
+        itemExists.quantity = +itemExists.quantity + +this.quantityValue;
+        oldCart.totalPrice =
+          +oldCart.totalPrice + +this.mealDetails.price * this.quantityValue;
+        oldCart.items[itemExists] = itemExists;
+        localStorage.setItem('cart', JSON.stringify(oldCart));
+      }
+      if (!itemExists) {
+        oldCart.items.push({
+          quantity: this.quantityValue,
+          id: this.mealDetails.id,
+          price: this.mealDetails.price,
+          name: this.mealDetails.name,
+          image: this.mealDetails.imageUrl,
+        });
+        oldCart.totalPrice =
+          +oldCart.totalPrice + +this.mealDetails.price * this.quantityValue;
+        localStorage.setItem('cart', JSON.stringify(oldCart));
+      }
     }
+
     this.quantityValue = 1;
   }
 }
